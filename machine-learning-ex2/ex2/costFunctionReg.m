625a3382bc3d7d98d73bf47f9d1374d04447cc95
Zhang Ran <supersnailszr@gmail.com>
2018-04-22 22:31:29 +0800

    # update regularized code

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -19,8 +19,19 @@ grad = zeros(size(theta));
 
 
 
+h = sigmoid(X*theta);
 
+J = (1 / m) * sum(-y .* log(h) - (1 - y) .* log(1-h)) + (lambda /(2*m)) * sum(theta(2:end) .^2);
 
+% the following code was refered from below link:
+%https://chameerawijebandara.wordpress.com/2014/07/18/logistic-regression-with-regularization-in-matlaboctave/
+
+grad(1) =   ( sum(( h - y ).* X( :,1 ))) / m ;
+for j = 2:length( theta )   
+ 
+    grad(j) =   ( sum(( h - y ).*X( :,j ))) / m + (lambda*theta(j))/m;
+ 
+end
 
 % =============================================================
 
