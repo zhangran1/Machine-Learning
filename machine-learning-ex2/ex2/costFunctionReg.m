2454917aa0711d5fedf0f55a4cf1290116e66dbb
zhangran1 <rzhang2@e.ntu.edu.sg>
2018-04-23 09:27:18 +0800

    # update indentation

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,8 +17,6 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
 h = sigmoid(X*theta);
 
 J = (1 / m) * sum(-y .* log(h) - (1 - y) .* log(1-h)) + (lambda /(2*m)) * sum(theta(2:end) .^2);
